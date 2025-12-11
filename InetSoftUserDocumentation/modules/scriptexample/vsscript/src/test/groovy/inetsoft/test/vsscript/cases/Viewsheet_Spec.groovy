8f4f5a241d5a99f85973956ec0cb9279cc80d496
frylockdoc <64930113+davidfass@users.noreply.github.com>
2025-12-11 15:49:21 -0500

    script testing infrastructure with ShapeFormSetPoint1.zip

--- InetSoftUserDocumentation/modules/scriptexample/vsscript/src/test/groovy/inetsoft/test/vsscript/cases/Viewsheet_Spec.groovy
+++ InetSoftUserDocumentation/modules/scriptexample/vsscript/src/test/groovy/inetsoft/test/vsscript/cases/Viewsheet_Spec.groovy
@@ -25,5 +25,21 @@ class Viewsheet_Spec extends Specification {
 
    }
 
+   def 'TestCase-ShapeFormSetPoint1' () {
+      given:
+      // Import the viewsheet under assets when needed
+      VSScriptTest.importAssets("ShapeFormSetPoint1.zip")
+
+      when:
+      vsScriptTest = new VSScriptTest('1%5E128%5E__NULL__%5EDocScriptExamples/ShapeFormSetPoint1%5Ehost-org', specificationContext.currentIteration.name)
+      // printVS(scriptName, jsFilePath, handler, assemblyNames)
+      // jsFilePath: relative path to JS file in partials directory (e.g., "AccessChartDataExp.js")
+      // handler: the HANDLER value (e.g., "ONREFRESH", "ONINIT", or assembly name)
+      vsScriptTest.printVS('ShapeFormSetPoint1', 'ShapeFormSetPoint1.js', 'Chart1', ['Chart1'] as String[])
+      then:
+      vsScriptTest.compareImage()
+
+   }
+
    static VSScriptTest vsScriptTest
 }
