6c5c75be7747782198fb81ad8179b6a32f5464b8
frylockdoc <64930113+davidfass@users.noreply.github.com>
2026-02-09 10:59:31 -0500

    update SampleFilter.java for Documentation #73811

--- InetSoftUserDocumentation/modules/ROOT/attachments/SampleFilter.java
+++ InetSoftUserDocumentation/modules/ROOT/attachments/SampleFilter.java
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright (c) 2019, InetSoft Technology Corp, All Rights Reserved.
  *
  * The software and information contained herein are copyrighted and
@@ -16,15 +16,15 @@ package inetsoft.sree;
 import java.io.IOException;
 import java.security.Principal;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import javax.servlet.http.HttpServletRequestWrapper;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpServletRequestWrapper;
 
 import inetsoft.sree.ClientInfo;
 import inetsoft.sree.RepletRepository;
